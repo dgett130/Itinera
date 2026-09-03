9bc2bba9be517a76cd06537b50f131ab25cf05aa
Diego Gettatelli <dgettatelli@gmail.com>
2026-09-03 18:49:30 +0200

    Sito pubblico: home, privacy, termini (per pubblicazione OAuth Google)

--- android/app/src/main/kotlin/org/itinera/itinera/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.itinera.itinera
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/id.dart
+++
@@ -1,6 +0,0 @@
-import 'package:uuid/uuid.dart';
-
-const _uuid = Uuid();
-
-/// Genera un nuovo identificatore univoco (UUID v4) per le chiavi primarie.
-String newId() => _uuid.v4();
